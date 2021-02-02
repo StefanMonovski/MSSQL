1edbe9e31ca964061c144fc1dfa0d285ca696b99
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-02-02 11:13:55 +0200

    Add SalesEmployees

---
+++ 09.SubqueriesAndJoins/03.SalesEmployees.sql
@@ -0,0 +1,5 @@
+SELECT E.EmployeeID, E.FirstName, E.LastName, D.Name AS DepartmentName
+    FROM Employees AS E
+    JOIN Departments AS D ON D.DepartmentID = E.DepartmentID
+    WHERE D.Name = 'Sales'
+    ORDER BY E.EmployeeID;
