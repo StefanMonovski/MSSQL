55d137d7f767c74af62ae4594900b73e832db6cc
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-02-02 12:36:06 +0200

    Add Employee24

---
+++ 09.SubqueriesAndJoins/08.Employee24.sql
@@ -0,0 +1,9 @@
+SELECT E.EmployeeID, E.FirstName,
+    CASE
+        WHEN DATEPART(YEAR, P.StartDate) >= 2005 THEN NULL
+        ELSE P.Name
+        END AS ProjectName
+    FROM Employees AS E
+    JOIN EmployeesProjects AS EP ON E.EmployeeID = EP.EmployeeID
+    JOIN Projects AS P ON P.ProjectID = EP.ProjectID
+    WHERE E.EmployeeID = 24;
