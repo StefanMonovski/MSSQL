5dd48c53cdb3f47f2683dc3f871c1223a3bac6ac
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-02-02 12:12:21 +0200

    Add EmployeesWithProject

---
+++ 09.SubqueriesAndJoins/07.EmployeesWithProject.sql
@@ -0,0 +1,6 @@
+SELECT TOP (5) E.EmployeeID, E.FirstName, P.Name AS ProjectName
+    FROM Employees AS E
+    JOIN EmployeesProjects AS EP ON E.EmployeeID = EP.EmployeeID
+    JOIN Projects AS P ON P.ProjectID = EP.ProjectID
+    WHERE P.StartDate > '2002-08-13' AND P.EndDate IS NULL
+    ORDER BY E.EmployeeID;
