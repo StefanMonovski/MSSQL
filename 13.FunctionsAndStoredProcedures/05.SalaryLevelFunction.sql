809624a77d1780986e04ff66d88e0df11745fb91
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-02-08 22:52:01 +0200

    Add SalaryLevelFunction

---
+++ 13.FunctionsAndStoredProcedures/05.SalaryLevelFunction.sql
@@ -0,0 +1,11 @@
+CREATE FUNCTION ufn_GetSalaryLevel(@salary DECIMAL(18, 4)) RETURNS VARCHAR(10) AS
+BEGIN
+    DECLARE @level VARCHAR(10)
+    IF (@salary < 30000)
+        SET @level = 'Low'
+    ELSE IF (@salary BETWEEN 30000 AND 50000)
+        SET @level = 'Average'
+    ELSE IF (@salary > 50000)
+        SET @level = 'High'
+    RETURN @level
+END;
