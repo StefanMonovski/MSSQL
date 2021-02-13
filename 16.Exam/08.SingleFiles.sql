058a52b5dfdd770b70b2700c6000654d6f13ca8e
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-02-13 12:26:15 +0200

    Add SingleFiles

---
+++ 16.Exam/08.SingleFiles.sql
@@ -0,0 +1,8 @@
+SELECT Id, Name, CONVERT(VARCHAR, Size) + 'KB' AS Size
+    FROM Files
+    WHERE Id NOT IN (
+        SELECT ParentId
+            FROM Files
+            WHERE ParentId IS NOT NULL
+        )
+    ORDER BY Id, Name, Size;
